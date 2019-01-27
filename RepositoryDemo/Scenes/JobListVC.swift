9ba310583028d42d60a1ad3a309a2545915eeb8c
kuotinyen <kuotinyen@gmail.com>
2019-01-27 21:54:19 +0800

    Add window root-vc with job-list-vc

---
+++ RepositoryDemo/Scenes/JobListVC.swift
@@ -0,0 +1,19 @@
+//
+//  JobListVC.swift
+//  RepositoryDemo
+//
+//  Created by TING YEN KUO on 2019/1/27.
+//  Copyright © 2019 TING YEN KUO. All rights reserved.
+//
+
+import UIKit
+
+class JobListVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+    }
+    
+}
